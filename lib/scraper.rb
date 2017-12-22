54194d2e315687df069f4ec590ec981d35e936db
Remmy Clay <remmy.lauren@gmail.com>
2017-12-22 22:41:34 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -19,9 +19,12 @@ class Scraper
 
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    binding.pry
   end
 
   def get_courses
 
   end
 end
+
+Scraper.new.get_page
