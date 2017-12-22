9a8565e4234e7d2b15e07cf1c308e9027974d712
Remmy Clay <remmy.lauren@gmail.com>
2017-12-22 22:39:54 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,11 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
 class Scraper
-  
+
     def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -21,9 +22,6 @@ class Scraper
   end
 
   def get_courses
-    
+
   end
 end
-
-
-
